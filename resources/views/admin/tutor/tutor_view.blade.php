368a2b46bda301b8100c75c819c8680d48ac22fa
Vishal_D_10 <mcavishal1111@gmail.com>
2022-05-20 01:42:27 +0530

    Add Confirmation Model and hide show Accept and rejected button and add status field

--- resources/views/admin/tutor/tutor_view.blade.php
+++ resources/views/admin/tutor/tutor_view.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
 @section('content')
+<link rel="stylesheet" href="{{ asset('assets/css/jquery-confirmation/css/jquery-confirm.min.css') }}">
     <div class="d-flex flex-column-fluid">
         <!--begin::Container-->
         <div class="container-fluid">
@@ -16,64 +17,70 @@
                                     <i class="mdi mdi-chevron-down d-none d-sm-inline-block"></i>
                                 </div>
                                 <div class="card-toolbar">
-                                    <a href="javascript:void(0);" class="btn btn-primary mr-2"
-                                        style="background-color:#34db5e !important  accept" onclick="changeStatus('Accept',{{$totuer->id}})">Accept</a>
-                                    <a href="javascript:void(0);" class="btn btn-primary mr-2"
-                                        style="background-color:#db3434 !important" onclick="changeStatus('Reject',{{$totuer->id}})">Reject</a>
+                                    
+
+                                    <a href="javascript:void(0);" class="btn btn-success mr-2 accepted_id"
+                                        onclick="changeStatus('Accepted',{{$tutor->id}})"  @if($tutor->status=="Accepted") style="display:none" @endif>Accept</a>
+                                    <a href="javascript:void(0);" class="btn btn-danger mr-2 rejected_id"
+                                         onclick="changeStatus('Rejected',{{$tutor->id}})" @if($tutor->status=="Rejected") style="display:none" @endif>Reject</a>
                                 </div>
                             </div>
                             <div class="card-body">
                                 <div class="form-group row">
                                     <div class="col-lg-4">
                                         <div class="d-flex mb-4">
-                                            <strong>Full Name:</strong>
-                                            {{ $totuer->first_name }}
+                                            <strong>Full Name: </strong>
+                                            {{ $tutor->first_name }}
                                         </div>
                                     </div>
                                     <div class="col-lg-4">
                                         <div class="d-flex mb-4">
-                                            <strong>Email:</strong>
-                                            {{ $totuer->email }}
+                                            <strong>Email: </strong> {{ $tutor->email }}
                                         </div>
                                     </div>
                                     <div class="col-lg-4">
                                         <div class="d-flex mb-4">
-                                            <strong>Mobile No:</strong>
-                                            {{ $totuer->mobile_id }}
+                                            <strong>Mobile No: </strong>
+                                            {{ $tutor->mobile_id }}
                                         </div>
                                     </div>
                                     <div class="col-lg-4">
                                         <div class="d-flex mb-4">
-                                            <strong>Address1:</strong>
-                                            {{ $totuer->address1 }}
+                                            <strong>Address1: </strong>
+                                            {{ $tutor->address1 }}
                                         </div>
                                     </div>
                                     <div class="col-lg-4">
                                         <div class="d-flex mb-4">
-                                            <strong>Address2:</strong>
-                                            {{ $totuer->address2 }}
+                                            <strong>Address2: </strong>
+                                            {{ $tutor->address2 }}
                                         </div>
                                     </div>
                                     <div class="col-lg-4">
                                         <div class="d-flex mb-4">
-                                            <strong>Address3:</strong>
-                                            {{ $totuer->address3 }}
+                                            <strong>Address3: </strong>
+                                            {{ $tutor->address3 }}
                                         </div>
                                     </div>
                                     <div class="col-lg-4">
-                                        <strong>City:</strong>
-                                        {{ $totuer->city }}
+                                        <strong>City: </strong>
+                                        {{ $tutor->city }}
                                     </div>
                                     <div class="col-lg-4">
                                         <div class="d-flex mb-4">
-                                            <strong>Post Code:</strong>
-                                            {{ $totuer->postcode }}
+                                            <strong>Post Code: </strong>  {{ $tutor->postcode }}
+                                           
                                         </div>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <strong>Status: </strong> <span id="status_id">@if($tutor->status =='Pending') <span class="badge badge-primary">Pending</span> @elseif($tutor->status =='Accepted') <span class="badge badge-success">Accepted</span> @else <span class="badge badge-danger">Rejected</span> @endif</span>
+                                        
                                     </div>
                                     <div class="col-lg-12">
                                         <strong>Bio:</strong>
-                                        {{ $totuer->bio }}
+                                        {{ $tutor->bio }}
                                     </div>
+                                    
                                 </div>
                             </div>
                         </div>
@@ -139,13 +146,14 @@
     
 @endsection
 @section('page-js')
+<script src="{{ asset('assets/js/pages/jquery-confirmation/js/jquery-confirm.min.js') }}"></script>
     <script>
         function getUniversityDetails(page) {
             $.ajax({
                 type: "GET",
                 url: "{{ route('tutor-university') }}",
                 data: {
-                    'tutor_id': '{{ $totuer->id }}',
+                    'tutor_id': '{{ $tutor->id }}',
                     'page': page,
                 },
                 success: function(res) {
@@ -161,7 +169,7 @@
                 type: "GET",
                 url: "{{ route('tutor-subject') }}",
                 data: {
-                    'tutor_id': '{{ $totuer->id }}',
+                    'tutor_id': '{{ $tutor->id }}',
                     'page': page,
                 },
                 success: function(res) {
@@ -177,7 +185,7 @@
                 type: "GET",
                 url: "{{ route('tutor-level-list') }}",
                 data: {
-                    'tutor_id': '{{ $totuer->id }}',
+                    'tutor_id': '{{ $tutor->id }}',
                     'page': page,
                 },
                 success: function(res) {
@@ -192,17 +200,58 @@
     </script>
     <script>
         function changeStatus(status,id) {
-          
-          $.ajax({
-                type: "GET",
-                url: "{{ route('changestatus') }}",
-                data: {
-                   id:id,
-                    status:status},
-                success: function(res) {
-                   
+            $.confirm({
+                title: 'Are you sure?',
+                columnClass:"col-md-6",
+
+                content: "you want to change status?",
+                buttons: {
+                    formSubmit: {
+                        text: 'Submit',
+                        btnClass: 'btn-primary',
+                        action: function () {
+                                $.ajax({
+                                    method: "GET",
+                                    url:"{{ route('changestatus') }}",
+                                    data:{
+                                       
+                                        
+                                        'id':id,
+                                        'status':status
+                                    }
+
+                                }).done(function (r) {
+
+                                    toastr.success(r.error_msg);
+                                    $('.rejected_id').attr('style','display:block');
+                                    $('.accepted_id').attr('style','display:block');
+                                    if( r.data.status =="Accepted"){
+                                        var html_res = '<span class="badge badge-success">Accepted</span>';
+                                        $('.accepted_id').attr('style','display:none');
+                                    }else{
+                                        var html_res = '<span class="badge badge-danger">Rejected</span>';
+                                        $('.rejected_id').attr('style','display:none');
+                                    }
+
+                                    $('#status_id').html(html_res);
+                                    
+                                }).fail(function () {
+                                    _self.setContent('Something went wrong. Contact Support.');
+                                    toastr.error('Sorry, something went wrong. Please try again.');
+                                });
+
+                        }
+                    },
+                    cancel: function () {
+                        //close
+                    },
+                },
+                onContentReady: function () {
+                    // bind to events
+
                 }
             });
+         
         }
         </script>
 @endsection
