63c3414ea0588cee8f52ed45f9d4391c3bc0e759
VishalDPatel <vishal.david@virtualheight.com>
2022-05-20 12:50:49 +0530

    Remove static url to convert dynamic url in admin side

--- resources/views/admin/elements/sidebar.blade.php
+++ resources/views/admin/elements/sidebar.blade.php
@@ -3,7 +3,7 @@
     <!--begin::Brand-->
     <div class="brand flex-column-auto" id="kt_brand">
         <!--begin::Logo-->
-        <a href="index.html" class="brand-logo">
+        <a href="{{ url('/admin')}}" class="brand-logo">
             <img alt="Logo" src="{{ asset('assets/media/logos/logo-light.png') }}" />
         </a>
         <!--end::Logo-->
