c5a27a325668971d0ff844862c70287deb751a3d
HarshadVH <harshaddabhi.vhits@gmail.com>
2022-05-20 10:25:07 +0530

    Remove Function in subject Master

--- app/Models/SubjectMaster.php
+++ app/Models/SubjectMaster.php
@@ -15,27 +15,5 @@ class SubjectMaster extends Model
     {
         return $this->belongsTo(SubjectMaster::class, 'id', $this->parent_id);
     }
-    public static function getAllData($subject, $level, $post_code)
-    {
-        $temp = '';
-        if ($subject != '') {
-            $temp .= ' main_title LIKE"' . '%' . $subject . '%' . '"';
-        }
-        if ($level != '') {
-            
-            $temp .= " AND sc_tutor_level.title LIKE '%" . $level . "%'";
-        }
-        // if ($post_code != '') {
-        //     $temp .= " AND FirstName LIKE '%" . $post_code . "%'";
-        // }
-        $query = SubjectMaster::select('main_title')
-            // ->leftjoin('sc_tutor_subject_details', function ($join) {
-            //     $join->on('sc_subject_master.id', '=', 'sc_tutor_subject_details.subject_id');
-            // })
-            // ->leftjoin('users', function ($join) {
-            //     $join->on('sc_tutor_subject_details.tutor_id', '=', 'users.id');
-            // })
-            ->whereRaw($temp)->toSql();
-        return $query;
-    }
+   
 }
