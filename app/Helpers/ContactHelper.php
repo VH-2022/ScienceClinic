97a9039adb01926e29dacb575c224c86d1f96bbd
NandiniDarji <nandinidarji.vhits@gmail.com>
2022-05-24 16:49:31 +0530

    Added | Admin Side Listing

--- app/Helpers/ContactHelper.php
+++ app/Helpers/ContactHelper.php
@@ -16,5 +16,28 @@ class ContactHelper
         $insert->save();
         $insertId = $insert->id;
         return $insertId;
-    }   
+    }  
+    
+    public static function getListwithPaginate($name,$phone_no,$tutor_type,$email,$created_date){
+
+        $query = Contact::whereNull('deleted_at');
+        if($name !=''){
+            $query->where('name','LIKE','%'.$name.'%');
+        }
+        if($phone_no !=''){
+            $query->where('phone_no','LIKE','%'.$phone_no.'%');
+        }
+        if($tutor_type !=''){
+            $query->where('tutor_type','LIKE','%'.$tutor_type.'%');
+        }
+        if($email !=''){
+            $query->where('email','LIKE','%'.$email.'%');
+        }
+        if($created_date !=''){
+            $explode = explode('-',$created_date);
+            $query->whereDate('created_at','>=',date('Y-m-d',strtotime($explode[0])))->whereDate('created_at','<=',date('Y-m-d',strtotime($explode[1])));
+        }
+        $query = $query->orderBy('id','desc')->paginate(10);
+        return $query;
+    }
 }
