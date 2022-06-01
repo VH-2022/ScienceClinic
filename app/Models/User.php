e0350308cd4f34611b16a276be09fce32469d89b
Parth Modi <parth.vhits@gmail.com>
2022-06-01 16:05:06 +0530

    Add softdelet option

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 use Illuminate\Notifications\Notifiable;
@@ -36,6 +36,7 @@ class User extends Authenticatable
 
     use TwoFactorAuthenticatable;
 
+    use SoftDeletes;
 
 
     /**
