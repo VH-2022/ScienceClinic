2cecce76e7b691d0a42c90bf35db14526c954624
HarshadVH <harshaddabhi.vhits@gmail.com>
2022-07-20 17:02:30 +0530

    Coorect Spelling

--- resources/views/admin/contact/contact.blade.php
+++ resources/views/admin/contact/contact.blade.php
@@ -18,7 +18,7 @@
                     <!--begin::Header-->
                     <div class="card-header py-3">
                         <div class="card-title align-items-start flex-column">
-                            <h3 class="card-label font-weight-bolder text-dark">Contact List</h3>
+                            <h3 class="card-label font-weight-bolder text-dark">Content List</h3>
                         </div>
                         <div class="card-toolbar">
                             <!--begin::Dropdown-->
